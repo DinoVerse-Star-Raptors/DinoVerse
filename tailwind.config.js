16a64a7a97cffb4713d778a590d37ed5507c9927
Thitikarn <thitikarn.ktpn@gmail.com>
2024-10-18 20:17:52 +0700

    file setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./index.html", "./all-product.html", "./product-info.html", "./cart.html", "./login.html", "./checkout.html", "./register.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
