276df7093944a757369c4de695335e8b907bfe25
Virachai Wongsena <wvirachai@gmail.com>
2024-10-21 13:43:49 +0700

    setup syntax react .jsx

--- register/main.jsx
+++ register/main.jsx
@@ -1,6 +1,28 @@
-import react from "@vitejs/plugin-react";
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+// import App from "./App";
+
+function App() {
+  return <div>Hello World!</div>;
+}
+
+const root = createRoot(document.querySelector("#app"));
+root && root.render(<App />);
+// ReactDOM.createRoot(document.getElementById("app")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 // import ReactDOM from "react-dom";
 // import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // ReactDOM.createRoot(document.querySelector("#app")).render(
 //   <React.StrictMode>
@@ -11,3 +33,32 @@ import react from "@vitejs/plugin-react";
 // document.querySelector("#app").innerHTML = (() => {
 //   return `Register in main.js`;
 // })();
+
+// Usage
+// In the browser
+// import { createRoot } from 'react-dom/client';
+
+// function App() {
+//   return <div>Hello World</div>;
+// }
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
+// On the server
+// import { renderToPipeableStream } from 'react-dom/server';
+
+// function App() {
+//   return <div>Hello World</div>;
+// }
+
+// function handleRequest(res) {
+//   // ... in your server handler ...
+//   const stream = renderToPipeableStream(<App />, {
+//     onShellReady() {
+//       res.statusCode = 200;
+//       res.setHeader('Content-type', 'text/html');
+//       stream.pipe(res);
+//     },
+//     // ...
+//   });
+// }
