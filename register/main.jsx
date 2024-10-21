6cc65befd90105f63fa0cd79e25af6d38de40981
Virachai Wongsena <wvirachai@gmail.com>
2024-10-21 13:15:33 +0700

    del reg in root dir

---
+++ register/main.jsx
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.querySelector("#app")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// document.querySelector("#app").innerHTML = (() => {
+//   return `Register in main.js`;
+// })();
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+/*
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
+        colmar: resolve(__dirname, "colmar/index.html"),
+        login: resolve(__dirname, "login/index.html"),
+      },
+    },
+  },
+});
+*/
